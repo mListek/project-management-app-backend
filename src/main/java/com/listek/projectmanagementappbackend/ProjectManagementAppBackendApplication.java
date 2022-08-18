a2eda7b73dd19a7781b49a7eb72cb8fb3814dca1
Michał Listek <michal.listex@gmail.com>
2022-08-18 20:28:11 +0200

    initialized project

---
+++ src/main/java/com/listek/projectmanagementappbackend/ProjectManagementAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.listek.projectmanagementappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagementAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectManagementAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/listek/projectmanagementappbackend/ProjectManagementAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.listek.projectmanagementappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagementAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
